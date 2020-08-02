ba58da00c216b38fad1ac0e56ab8877db488fcfe
kevv87 <kev_sala@outlook.com>
2020-08-02 12:04:54 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const productRouter = require('./routers/product')
 const productorRouter = require('./routers/productor')
 const regionRouter = require('./routers/region')
 const transporteRouter = require('./routers/transporte')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(productRouter)
